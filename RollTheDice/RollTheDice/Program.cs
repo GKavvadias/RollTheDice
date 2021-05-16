0f565252cdab7d350d93e1e97639b4d2e5720257
GKavvadias <80004643+GKavvadias@users.noreply.github.com>
2021-05-16 17:36:32 +0300

    Add files via upload

---
+++ RollTheDice/RollTheDice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollTheDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("=======================");
+            Console.WriteLine("ROLLING A DICE 10 TIMES");
+            Console.WriteLine("=======================");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 7) + " " + random.Next(1, 7));
+                Console.WriteLine("-----------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
